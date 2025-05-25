3f5646e66d88a87c2303ceba46457b0bda7469be
Sahan Dias <sahankaushalya1102@gmail.com>
2025-05-25 23:24:09 +0530

    updated rapp_routes.dart file

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,27 +1,40 @@
-import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:flutter/material.dart'; // Required for context
 import 'package:teasypay_kd/views/offer_details_screen.dart';
 import 'package:teasypay_kd/views/search_train_screen.dart';
 import 'package:teasypay_kd/views/select_your_train_screen.dart';
 import 'package:teasypay_kd/views/train_details_screen.dart';
 
 class AppRoutes {
-  static const String searchTrain = '/searchTrain';
-  static const String selectTrain = "/selectTrain";
-  static const String trainDetails = "/trainDetails";
-  static const String offerDetails = "/offerDetails";
-
-  static Route<dynamic> generateRoute(RouteSettings settings) {
-    switch (settings.name) {
-      case searchTrain:
-        return MaterialPageRoute(builder: (_) => SearchTrainScreen());
-      case selectTrain:
-        return MaterialPageRoute(builder: (_) => SelectTrainScreen());
-      case trainDetails:
-        return MaterialPageRoute(builder: (_) => TrainDetailsScreen());
-      case offerDetails:
-        return MaterialPageRoute(builder: (_) => OfferDetailsScreen());
-      default:
-        return MaterialPageRoute(builder: (_) => SearchTrainScreen());
-    }
-  }
+  static final GoRouter router = GoRouter(
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) => const SearchTrainScreen(),
+      ),
+      GoRoute(
+        path: '/selectTrain',
+        builder: (context, state) {
+          final searchData = state.extra as Map<String, dynamic>?;
+          if (searchData == null) {
+            return const Scaffold(
+              body: Center(child: Text("No search data provided")),
+            );
+          }
+          return SelectTrainScreen(searchData: searchData);
+        },
+      ),
+      GoRoute(
+        path: '/trainDetails/:trainId',
+        builder: (context, state) {
+          final trainId = state.pathParameters['trainId']!;
+          return TrainDetailsScreen(trainId: trainId);
+        },
+      ),
+      GoRoute(
+        path: '/offerDetails',
+        builder: (context, state) => const OfferDetailsScreen(),
+      ),
+    ],
+  );
 }
